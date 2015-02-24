47f1dddbce2eb8b1092f79f3032c2cce85adb6fa
popstarfreas <popstarfreas2@gmail.com>
2015-02-24 22:15:12 +0000

    Added a missing anchor end tag

--- display_users.php
+++ display_users.php
@@ -12,7 +12,7 @@ $list = '<div id="players">';
 foreach ($player['list'] as $player) {
     if (is_array($player)) {
         foreach ($player as $p) {
-            if (!empty($p)) $list .= '<a href="?player=' . str_replace('#', '%23', $p['nickname']) . '">' . $p['nickname'] . '<br />';
+            if (!empty($p)) $list .= '<a href="?player=' . str_replace('#', '%23', $p['nickname']) . '">' . $p['nickname'] . '</a><br />';
         }
     }
 }
